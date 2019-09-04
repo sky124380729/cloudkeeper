f763859e22cfb118944d9a7dafe8df0d91fc4159
pink <jerry.zheng@cloudkeeper.cn>
2019-09-04 18:04:39 +0800

    das

--- pages/news.vue
+++ pages/news.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        新闻中心
+    <div class="news">
+        <h3>新闻中心</h3>
+        <div class="news-container"></div>
     </div>
 </template>
 
@@ -14,4 +15,14 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.news {
+    h3 {
+        font-size: 18px;
+        font-weight: bold;
+        color: rgba(48, 49, 51, 1);
+        line-height: 18px;
+        margin-bottom: 20px;
+    }
+}
+</style>
